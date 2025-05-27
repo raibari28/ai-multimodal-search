92eba41c920a75d5513ab71309c10d83a33a23fa
B215 <raibari28@gmail.com>
2025-05-27 08:47:45 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,13 +3,17 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from mangum import Mangum
 
+# Create FastAPI app
 app = FastAPI()
 
+# Define request schema
 class RequestBody(BaseModel):
     query: str
 
+# Define route
 @app.post("/search")
 async def search(body: RequestBody):
     return JSONResponse(content={"response": f"You searched for: {body.query}"})
 
+# Define AWS Lambda-compatible handler
 handler = Mangum(app)
