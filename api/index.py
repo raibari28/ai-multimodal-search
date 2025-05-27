5a9a4d95d0f6621957345e0c1a188dea57641d06
B215 <raibari28@gmail.com>
2025-05-27 08:58:29 +0700

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -3,17 +3,14 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from mangum import Mangum
 
-# Create FastAPI app
 app = FastAPI()
 
-# Define request schema
 class RequestBody(BaseModel):
     query: str
 
-# Define route
 @app.post("/search")
 async def search(body: RequestBody):
     return JSONResponse(content={"response": f"You searched for: {body.query}"})
 
-# Define AWS Lambda-compatible handler
+# AWS Lambda-compatible handler for Vercel
 handler = Mangum(app)
